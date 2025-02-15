3cfc8200ba31ea847c9dfcad19ea2c04a8dd08bb
Joostlek <joostlek@outlook.com>
2025-02-15 12:37:54 +0100

    Initial commit

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import os
 from pathlib import Path
 
@@ -6,14 +7,18 @@ import orjson
 from pysmartthings import Capability, SmartThings
 import yaml
 
+LOGGER = logging.getLogger(__name__)
+
 async def main():
+    LOGGER.info("Downloading capabilities")
     json_root = Path("json")
     json_root.mkdir(exist_ok=True)
     yaml_root = Path("yaml")
     yaml_root.mkdir(exist_ok=True)
     async with SmartThings() as client:
-        client.authenticate("")
-        for capability in [Capability.VALVE]:
+        client.authenticate(os.getenv("SMARTTHINGS_TOKEN"))
+        for capability in Capability:
+            LOGGER.info("Downloading %s", capability)
             standard_namespace = "." not in capability
             namespace = "standard" if standard_namespace else capability.split(".")[0]
             schema = await client.get_capability(capability)
@@ -24,6 +29,7 @@ async def main():
             yaml_path.mkdir(exist_ok=True)
             with open(yaml_path / f"{capability}.yaml", "w") as f:
                 yaml.dump(orjson.loads(schema), f, sort_keys=False)
+            LOGGER.info("Downloaded %s", capability)
             await asyncio.sleep(1)
 
     os.system("npm run prettier")
